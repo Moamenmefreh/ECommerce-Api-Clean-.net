2f881cd1cc28c2387fe8a074e830135a72eaa3e0
Moamen <moamenmefre@gmail.com>
2026-08-05 13:57:40 +0300

    Add EcommerceAPI project files after removing submodule

---
+++ src/EcommerceAPI/Controllers/OrdersController.cs
@@ -0,0 +1,51 @@
+﻿using Ecommerce.Application.Order.OrderCommands.CreateOrder;
+using Ecommerce.Application.Orders.OrderCommands.CancelOrder;
+using Ecommerce.Application.Orders.OrderQueries.GetOrderById;
+using Ecommerce.Application.Orders.OrderQueries.GetUserOrders;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+namespace Ecommerce.API.Controllers
+{
+    //[Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController(ISender sender) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder([FromBody] CreateOrderCommand command)
+        {
+            var result = await sender.Send(command);
+            return Ok(result);
+        }
+        [HttpPut("{id:guid}/cancel")]
+        public async Task<IActionResult> CancelOrder([FromRoute] Guid id)
+        {
+            CancelOrderCommand command = new CancelOrderCommand { OrderId = id };
+            var result = await sender.Send(command);
+
+            return Ok(result);
+        }
+        [HttpGet("GetOrder/{id:guid}")]
+        public async Task<IActionResult> GetOrder([FromRoute] Guid id)
+        {
+            GetOrderByIdQuery query = new GetOrderByIdQuery
+            {
+                OrderId = id,
+            };
+            var result = await sender.Send(query);
+            return Ok(result);
+        }
+        [HttpGet("user/{userId:guid}")]
+        public async Task<IActionResult> GetUserOrders(Guid userId)
+        {
+            var result = await sender.Send(new GetUserOrdersQuery
+            {
+                UserId = userId
+            });
+
+            return Ok(result);
+
+        }
+    }
+}
---
+++ src/EcommerceAPI/Controllers/ProductsController.cs
@@ -0,0 +1,61 @@
+﻿using Ecommerce.Application.Products.ProductCommands.CreateProduct;
+using Ecommerce.Application.Products.ProductCommands.DeleteProduct;
+using Ecommerce.Application.Products.ProductCommands.UpdateProduct;
+using Ecommerce.Application.Products.ProductQueries.GetAll;
+using Ecommerce.Application.Products.ProductQueries.GetById;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace Ecommerce.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController(ISender sender) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> AddProduct([FromBody] CreateProductCommand command)
+    {
+        var result = await sender.Send(command);
+        return Ok(result);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteProduct([FromRoute] Guid id)
+    {
+        DeleteProductCommand command = new DeleteProductCommand { ProductId = id };
+
+        var result = await sender.Send(command);
+        return Ok(result);
+    }
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> UpdateProduct([FromRoute] Guid id, [FromBody] UpdateProductCommand command)
+    {
+        command.ProductId = id;
+        var result = await sender.Send(command);
+        return Ok(result);
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetAllProducts([FromQuery] string? name, [FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
+    {
+        var query = new GetAllQueries
+        {
+            ProductName = name,
+            PageNumber = pageNumber,
+            PageSize = pageSize
+        };
+        var result = await sender.Send(query);
+        return Ok(result);
+    }
+
+    [HttpGet("{id:guid}")]
+
+    public async Task<IActionResult> GetById([FromRoute] Guid id)
+    {
+        GetByIdQuery command=new GetByIdQuery { ProductId = id };
+        var result = await sender.Send(command);
+        return Ok(result);
+    }
+}
